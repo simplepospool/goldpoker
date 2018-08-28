4f9a5b98e60efdcaca4f26e0e8fd9e9379739810
simplepospool <38740186+simplepospool@users.noreply.github.com>
2018-08-28 22:05:27 +0100

    Create bws_bootstrap.sh

---
+++ bws_bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+ cd
+ systemctl stop BWS.service
+ sleep 60
+ cd .bws
+ rm -rf blocks
+ rm -rf backups
+ rm -rf chainstate
+ rm -rf database
+ rm -rf sporks
+ rm -rf zerocoin
+ rm db.log
+ rm debug.log
+ rm peers.dat
+ wget https://www.dropbox.com/s/zse6sk1bl4aghjd/bws_boot.zip
+ unzip bws_boot.zip
+ rm bws_boot.zip
+ cd
