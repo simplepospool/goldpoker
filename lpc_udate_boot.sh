15686e732b3f37bb9ee8aa8bb2e4af4de8b6e814
simplepospool <38740186+simplepospool@users.noreply.github.com>
2018-09-15 19:29:21 +0100

    Update lpc_udate_boot.sh

--- lpc_udate_boot.sh
+++ lpc_udate_boot.sh
@@ -6,7 +6,7 @@ cd .lightpaycoin
 rm -f peers.dat
 rm -rf blocks
 rm -rf chainstate
-wget https://www.dropbox.com/s/qj9eanp05g5p35b/lpc_bootstrap.zip
+wget https://www.dropbox.com/s/v58w3t9jkyj6k5y/lpc_bootstrap.zip
 unzip lpc_bootstrap.zip
 rm lpc_bootstrap.zip
 systemctl start LightPayCoin.service
