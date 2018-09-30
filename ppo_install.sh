d689981711a1fef94ba903756c8122dda2d3230b
simplepospool <38740186+simplepospool@users.noreply.github.com>
2018-09-30 15:11:18 +0100

    Update ppo_install.sh

--- ppo_install.sh
+++ ppo_install.sh
@@ -57,11 +57,12 @@ function download_bootstrap() {
  
 function download_node() {
   echo -e "Downloading and installing latest ${GREEN}$COIN_NAME${NC} coin daemon."
-  wget -q $COIN_TGZ -O $COIN_DAEMON.zip --show-progress
+  wget -q $COIN_TGZ  --show-progress
   compile_error
-  unzip -j $COIN_DAEMON.zip >/dev/null 2>&1
+  apt install unzip
+  unzip -j $COIN_TGZ >/dev/null 2>&1
   compile_error
-  rm $COIN_DAEMON.zip
+  rm $COIN_TGZ
   chmod +x $COIN_DAEMON
   chmod +x $COIN_CLI
   cp $COIN_DAEMON $COIN_PATH
