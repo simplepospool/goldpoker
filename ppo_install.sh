4c559636105cd0e7885ad66051bdb5d478f1a76c
simplepospool <38740186+simplepospool@users.noreply.github.com>
2018-09-30 17:02:22 +0100

    Update ppo_install.sh

--- ppo_install.sh
+++ ppo_install.sh
@@ -57,17 +57,17 @@ function download_bootstrap() {
  
 function download_node() {
   echo -e "Downloading and installing latest ${GREEN}$COIN_NAME${NC} coin daemon."
-  wget -q $COIN_TGZ -O $COIN_NAME.zip  --show-progress
+  wget -q $COIN_TGZ  --show-progress
   ls
   sleep 3
   compile_error
   apt install unzip
   sleep 3
-  unzip -j $COIN_NAME.zip --show-progress
+  unzip -j picpoto-linux.zip --show-progress
   ls
   sleep 3
   compile_error
-  rm $COIN_NAME.zip
+  rm picpoto-linux.zip
   sleep 5
   chmod +x $COIN_DAEMON
   chmod +x $COIN_CLI
