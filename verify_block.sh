b16194d703c945b33b5cee9b6f9f44d0aefbc870
simplepospool <38740186+simplepospool@users.noreply.github.com>
2018-11-15 17:48:08 +0000

    Update verify_block.sh

--- verify_block.sh
+++ verify_block.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-a=$(logiscoin-cli getblockhash 132367)
+a=$(gpkr-cli getblockhash 187452)
 
 echo $a
 
-if [ $a = 844c98115bdd20b4c507f1497ff4a8c0ce587acf073ded6568912b240dc56813 ]
-  then echo "Esta na blockchain certa"
-  logiscoin-cli masternode status
+if [ $a = 3e60021b09174b682b213ca962405ef0c88ebb3fdf86393a3cedf19890504acd ]
+  then echo "You´re on the right chain"
+  gpkr-cli masternode status
  
 
 else
-  echo "Vamos colocar na blockchain correta"
-  systemctl stop LogisCoin.service
-  killall logiscoind
-  cd .logiscoin
-  rm -rf backups
-  rm -rf chainstate
-  rm *.dat
-  rm -rf database
-  rm -rf zerocoin
-  rm -rf blocks
-  rm *.log
-  rm logiscoind.pid
-  rm masternode.conf
-  rm -rf sporks
-  wget https://www.dropbox.com/s/6bletncz1bnupv9/lgs_bootstrap.zip
-  unzip lgs_bootstrap.zip
-  rm lgs_bootstrap.zip
-  systemctl start LogisCoin.service
-  sleep 60
-  logiscoin-cli getinfo
-      
+  echo "You´re on the wrong chain"
+ 
 fi
