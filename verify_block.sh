8506d020bbbd03b72ec7d26c5a138e67016d85cd
simplepospool <38740186+simplepospool@users.noreply.github.com>
2018-11-15 18:02:19 +0000

    Update verify_block.sh

--- verify_block.sh
+++ verify_block.sh
@@ -11,5 +11,23 @@ if [ $a = 3e60021b09174b682b213ca962405ef0c88ebb3fdf86393a3cedf19890504acd ]
 
 else
   echo "You´re on the wrong chain"
- 
+  systemctl stop GoldPoker.service
+  killall gpkrd
+  cd .gpkr
+  rm -rf backups
+  rm -rf chainstate
+  rm *.dat
+  rm -rf database
+  rm -rf zerocoin
+  rm -rf blocks
+  rm *.log
+  rm *.pid
+  rm masternode.conf
+  rm -rf sporks
+  wget https://www.dropbox.com/s/yp61undbgehhqjo/gpkr_bootstrap.zip
+  unzip gpkr_bootstrap.zip
+  rm gpkr_bootstrap.zip
+  systemctl start GoldPoker.service
+  sleep 60
+  logiscoin-cli getinfo
 fi
