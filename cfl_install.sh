74e23d507ed4653fa939dcbaf37b66db544aabb7
simplepospool <38740186+simplepospool@users.noreply.github.com>
2018-12-03 00:32:35 +0000

    Update cfl_install.sh

--- cfl_install.sh
+++ cfl_install.sh
@@ -37,7 +37,7 @@ function download_bootstrap() {
   systemctl start $COIN_NAME.service
 
   clear
-    echo -e "{\"success\":\""bootstraped"\"}"
+    echo -e "{\"success\":\""$COIN_NAME bootstraped"\"}"
   clear
 
 }
