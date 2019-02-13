1edcb88d02b2a3ee4144d6b522b4ebabd0b7afbe
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-02-13 19:26:38 +0000

    Update lgs_install.sh

--- lgs_install.sh
+++ lgs_install.sh
@@ -39,23 +39,17 @@ progressfilt () {
 }
 
 function download_bootstrap() {
-  systemctl stop $COIN_NAME.service
-  sleep 60
-  apt install unzip
-  cd
-  cd $CONFIGFOLDER
-  rm -rf blocks
-  rm -rf chainstate
-  rm peers.dat
-  wget $BOOTSTRAP
-  unzip $BOOTSTRAP_ZIP
-  rm $BOOTSTRAP_ZIP
-  cd
-  systemctl start $COIN_NAME.service
-
+  rm -rf $CONFIGFOLDER/blocks >/dev/null 2>&1
+  rm -rf $CONFIGFOLDER/chainstate >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.pid >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.log >/dev/null 2>&1
+  wget -q $BOOTSTRAP
+  unzip -oq $BOOTSTRAP_FILE -d $CONFIGFOLDER
+ 
   clear
-    echo -e "{\"success\":\""$COIN_NAME bootstraped"\"}"
-  clear
+    #echo -e "{\"success\":\""$COIN_NAME bootstraped"\"}"
+  #clear
 
 }
 
@@ -308,6 +302,7 @@ function important_information() {
 function setup_node() {
   get_ip
   create_config
+  download_bootstrap
   create_key
   update_config
   enable_firewall
@@ -327,4 +322,4 @@ checks
 prepare_system
 compile_node
 setup_node
-download_bootstrap
+
