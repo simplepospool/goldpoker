c6292618510b54a4b45cf5d9320382b7fd2232bb
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-02-15 20:19:26 +0000

    Update cfl_install.sh

--- cfl_install.sh
+++ cfl_install.sh
@@ -11,7 +11,7 @@ COIN_NAME='CryptoFlow'
 COIN_PORT=3333
 RPC_PORT=3334
 BOOTSTRAP='https://www.dropbox.com/s/qmg7xox4esp4pvn/cfl_bootstrap.zip'
-BOOTSTRAP_ZIP='cfl_bootstrap.zip'
+BOOTSTRAP_FILE='cfl_bootstrap.zip'
  
 NODEIP=$(curl -s4 icanhazip.com)
  
