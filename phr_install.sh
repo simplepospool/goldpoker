d39cd1ae3ed20d70cd414e11bf9f6f370e03c0e3
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-02-20 11:29:09 +0000

    Update phr_install.sh

--- phr_install.sh
+++ phr_install.sh
@@ -6,12 +6,13 @@ CONFIGFOLDER='/root/.phore'
 COIN_DAEMON='phored'
 COIN_CLI='phore-cli'
 COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/phoreproject/Phore/releases/download/v1.5.1/phore-1.5.1-x86_64-linux-gnu.tar.gz'
-COIN_NAME='Phore'
+COIN_TGZ='https://github.com/phoreproject/Phore/releases/download/v1.6.1/phore-1.6.1-x86_64-linux-gnu.tar.gz'
+COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
+COIN_NAME='phore'
 COIN_PORT=11771
 RPC_PORT=11770
 BOOTSTRAP='https://www.dropbox.com/s/h5eci8alr60cssv/phr_bootstrap.zip'
-BOOTSTRAP_FILE='phr_bootstrap.zip'
+BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
  
 NODEIP=$(curl -s4 icanhazip.com)
  
@@ -22,38 +23,32 @@ GREEN=''
 NC=''
 
 function download_bootstrap() {
-  systemctl stop $COIN_NAME.service
-  sleep 60
-  apt install unzip
-  cd $CONFIGFOLDER
-  rm -rf blocks
-  rm -rf chainstate
-  rm peers.dat
-  wget -N $BOOTSTRAP
-  unzip $BOOTSTRAP_FILE
+  rm -rf $CONFIGFOLDER/blocks >/dev/null 2>&1
+  rm -rf $CONFIGFOLDER/chainstate >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.pid >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.log >/dev/null 2>&1
+  wget -q $BOOTSTRAP
+  unzip -oq $BOOTSTRAP_FILE -d $CONFIGFOLDER
   rm $BOOTSTRAP_FILE
-  cd
-  systemctl start $COIN_NAME.service
-  sleep 60
+ 
+  clear
+    #echo -e "{\"success\":\""$COIN_NAME bootstraped"\"}"
+  #clear
 
-  clear
-    echo -e "{\"success\":\""$COIN_NAME bootstraped"\"}"
-  clear
-  
-  }
+}
  
 function download_node() {
-  echo -e "Downloading and installing latest ${GREEN}$COIN_NAME${NC} coin daemon."
+  echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
   cd $TMP_FOLDER >/dev/null 2>&1
-  wget -qO- $COIN_TGZ | tar xvz
+  wget -q $COIN_TGZ
   compile_error
-  rm $COIN_TGZ
-  cd phor*
-  cd bin
-  chmod +x *
-  cp $COIN_DAEMON $COIN_PATH
-  cp $COIN_CLI $COIN_PATH
-  cd ~ >/dev/null 2>&1
+  tar xvf $COIN_ZIP || unzip $COIN_ZIP >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_DAEMON) $COIN_PATH >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_CLI) $COIN_PATH >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_DAEMON >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_CLI >/dev/null 2>&1
+  cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
 }
@@ -117,11 +112,13 @@ EOF
 }
  
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME Masternode GEN Key${NC}. Or Press enter generate New Genkey"
   read -t 10 -e COINKEY
   if [[ -z "$COINKEY" ]]; then
   $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
+  while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+  done
   if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
    echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
    exit 1
@@ -129,8 +126,10 @@ function create_key() {
   COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
   if [ "$?" -gt "0" ];
     then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-    sleep 30
+    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the GEN Key${NC}"
+    while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+    done
     COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
   fi
   $COIN_PATH$COIN_CLI stop
@@ -146,12 +145,7 @@ masternode=1
 masternodeaddr=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
 bantime=60
-addnode=52.26.253.109
-addnode=52.11.62.231
-addnode=34.219.150.132
-addnode=34.219.45.27
-addnode=54.244.8.88
-addnode=52.26.212.126
+
 EOF
 }
  
@@ -275,6 +269,7 @@ clear
 function setup_node() {
   get_ip
   create_config
+  download_bootstrap
   create_key
   update_config
   enable_firewall
@@ -290,4 +285,4 @@ checks
 prepare_system
 download_node
 setup_node
-download_bootstrap
+
