29c7bfe65b64e41fdb291102f16df52b7aecbdf3
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-02-20 00:22:52 +0000

    Update vulc_install.sh

--- vulc_install.sh
+++ vulc_install.sh
@@ -7,11 +7,12 @@ COIN_DAEMON='vulcanod'
 COIN_CLI='vulcano-cli'
 COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/VulcanoCrypto/Vulcano/releases/download/v2.0.0.0/vulcano-2.0.0.0-linux64.tar.gz'
+COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='Vulcano'
 COIN_PORT=62543
 RPC_PORT=62542
 BOOTSTRAP='https://www.dropbox.com/s/mt1op3rcku35bb6/vulc_bootstrap.zip'
-BOOTSTRAP_ZIP='vulc_bootstrap.zip'
+BOOTSTRAP_FILE=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
  
 NODEIP=$(curl -s4 icanhazip.com)
  
@@ -23,23 +24,18 @@ NC=''
 
 
 function download_bootstrap() {
-  systemctl stop $COIN_NAME.service
-  sleep 60
-  apt install unzip
-  cd
-  cd $CONFIGFOLDER
-  rm -rf blocks
-  rm -rf chainstate
-  rm peers.dat
-  wget -N $BOOTSTRAP
-  unzip $BOOTSTRAP_ZIP
-  rm $BOOTSTRAP_ZIP
-  cd
-  systemctl start $COIN_NAME.service
-
+  rm -rf $CONFIGFOLDER/blocks >/dev/null 2>&1
+  rm -rf $CONFIGFOLDER/chainstate >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.pid >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/*.log >/dev/null 2>&1
+  wget -q $BOOTSTRAP
+  unzip -oq $BOOTSTRAP_FILE -d $CONFIGFOLDER
+  rm $BOOTSTRAP_FILE
+ 
   clear
-    echo -e "{\"success\":\""bootstraped"\"}"
-  clear
+    #echo -e "{\"success\":\""$COIN_NAME bootstraped"\"}"
+  #clear
 
 }
 
@@ -58,15 +54,16 @@ purgeOldInstallation() {
 }
  
 function download_node() {
-  echo -e "Downloading and installing latest ${GREEN}$COIN_NAME${NC} coin daemon."
+  echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
   cd $TMP_FOLDER >/dev/null 2>&1
-  wget -qO- $COIN_TGZ | tar xvz
+  wget -q $COIN_TGZ
   compile_error
-  rm $COIN_TGZ
-  chmod +x *
-  cp $COIN_DAEMON $COIN_PATH
-  cp $COIN_CLI $COIN_PATH
-  cd ~ >/dev/null 2>&1
+  tar xvf $COIN_ZIP || unzip $COIN_ZIP >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_DAEMON) $COIN_PATH >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_CLI) $COIN_PATH >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_DAEMON >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_CLI >/dev/null 2>&1
+  cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
 }
@@ -131,11 +128,13 @@ EOF
 }
  
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME Masternode GEN Key${NC}. Or Press enter generate New Genkey"
   read -t 10 -e COINKEY
   if [[ -z "$COINKEY" ]]; then
   $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
+  while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+  done
   if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
    echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
    exit 1
@@ -143,8 +142,10 @@ function create_key() {
   COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
   if [ "$?" -gt "0" ];
     then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-    sleep 30
+    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the GEN Key${NC}"
+    while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+    done
     COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
   fi
   $COIN_PATH$COIN_CLI stop
@@ -288,6 +289,7 @@ clear
 function setup_node() {
   get_ip
   create_config
+  download_bootstrap
   create_key
   update_config
   enable_firewall
@@ -299,9 +301,9 @@ function setup_node() {
 ##### Main #####
 clear
 
-purgeOldInstallation
+#purgeOldInstallation
 checks
 prepare_system
 download_node
 setup_node
-download_bootstrap
+
