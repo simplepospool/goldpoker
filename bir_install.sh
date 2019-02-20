dd315f4ca2c4d63047a6519459687679ff8c846d
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-02-20 22:35:34 +0000

    Update bir_install.sh

--- bir_install.sh
+++ bir_install.sh
@@ -7,13 +7,12 @@ COIN_DAEMON='birakecoind'
 COIN_CLI='birakecoin-cli'
 COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/birake/birakecoin/releases/download/v1.0.0.1/birakecoin-1.0.1-x86_64-linux-gnu.tar.gz'
-COIN_TGZ_FILE='birakecoin-1.0.1-x86_64-linux-gnu.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='birakecoin'
 COIN_PORT=39697
 RPC_PORT=39698
 BOOTSTRAP='https://www.dropbox.com/s/zaxwqcnlgh3i5yk/bir_bootstrap.zip'
-BOOTSTRAP_FILE='bir_bootstrap.zip'
+BOOTSTRAP_FILE=COIN_ZIP=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
 
@@ -71,18 +70,13 @@ function install_sentinel() {
 function download_node() {
   echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
   cd $TMP_FOLDER >/dev/null 2>&1
-  wget $COIN_TGZ
+  wget -q $COIN_TGZ
   compile_error
-  tar -zvxf $COIN_TGZ_FILE
-  cd birak*
-  cd bin
-  chmod +x $COIN_DAEMON
-  chmod +x $COIN_CLI
-  strip $COIN_DAEMON
-  strip $COIN_CLI
-  compile_error
-  cp $COIN_DAEMON $COIN_PATH
-  cp $COIN_CLI $COIN_PATH
+  tar xvf $COIN_ZIP || unzip $COIN_ZIP >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_DAEMON) $COIN_PATH >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_CLI) $COIN_PATH >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_DAEMON >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_CLI >/dev/null 2>&1
   cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
@@ -147,7 +141,9 @@ function create_key() {
   read -t 10 -e COINKEY
   if [[ -z "$COINKEY" ]]; then
   $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
+  while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+  done
   if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
    echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
    exit 1
@@ -156,7 +152,9 @@ function create_key() {
   if [ "$?" -gt "0" ];
     then
     echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the GEN Key${NC}"
-    sleep 30
+    while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+    done
     COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
   fi
   $COIN_PATH$COIN_CLI stop
