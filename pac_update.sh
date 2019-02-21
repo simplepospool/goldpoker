eb16c5e69af2e1faf227ccc3cf8add9bb01476d9
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-02-21 10:21:17 +0000

    Update pac_update.sh

--- pac_update.sh
+++ pac_update.sh
@@ -6,9 +6,9 @@ if [ -f /etc/systemd/system/pacd.service ]; then
     sleep 20
     cd
     rm pacc*
-    wget https://github.com/PACCommunity/PAC/releases/download/v0.12.5.1/PAC-v0.12.5.1-linux-x86_64.tar.gz
-    tar -zvxf PAC-v0.12.5.1-linux-x86_64.tar.gz
-    rm PAC-v0.12.5.1-linux-x86_64.tar.gz
+    wget -q https://github.com/PACCommunity/PAC/releases/download/v0.12.6.0/PAC-v0.12.6.0-linux-x86_64.tar.gz
+    tar -zvxf PAC-v0.12.6.0-linux-x86_64.tar.gz
+    rm PAC-v0.12.6.0-linux-x86_64.tar.gz
     chmod +x pac*
     cd .paccoincore
     rm -rf backups
@@ -17,13 +17,13 @@ if [ -f /etc/systemd/system/pacd.service ]; then
     rm *.log
     rm -rf blocks
     rm -rf chainstate
-    wget https://www.dropbox.com/s/oaxp7875wy8kxpp/pac_bootstrap.zip
-    unzip pac_bootstrap.zip
+    wget -q https://www.dropbox.com/s/oaxp7875wy8kxpp/pac_bootstrap.zip
+    unzip -oq pac_bootstrap.zip
     rm pac_bootstrap.zip
     cd
     systemctl start pacd.service
-    sleep 60
-    ./paccoin-cli getinfo
+    # sleep 60
+    # ./paccoin-cli getinfo
     echo "Atualizado Servico Pac geral" 
 elif [ -f /etc/systemd/system/Paccoin.service ]; then 
     systemctl stop Paccoin.service
@@ -31,9 +31,9 @@ elif [ -f /etc/systemd/system/Paccoin.service ]; then
     sleep 20
     cd /usr/local/bin
     rm pacc*
-    wget https://github.com/PACCommunity/PAC/releases/download/v0.12.5.1/PAC-v0.12.5.1-linux-x86_64.tar.gz
-    tar -zvxf PAC-v0.12.5.1-linux-x86_64.tar.gz
-    rm PAC-v0.12.5.1-linux-x86_64.tar.gz
+    wget -q https://github.com/PACCommunity/PAC/releases/download/v0.12.6.0/PAC-v0.12.6.0-linux-x86_64.tar.gz
+    tar -zvxf PAC-v0.12.6.0-linux-x86_64.tar.gz
+    rm PAC-v0.12.6.0-linux-x86_64.tar.gz
     chmod +x pac*
     cd
     cd .paccoincore
@@ -43,22 +43,22 @@ elif [ -f /etc/systemd/system/Paccoin.service ]; then
     rm *.log
     rm -rf blocks
     rm -rf chainstate
-    wget https://www.dropbox.com/s/oaxp7875wy8kxpp/pac_bootstrap.zip
-    unzip pac_bootstrap.zip
+    wget -q https://www.dropbox.com/s/oaxp7875wy8kxpp/pac_bootstrap.zip
+    unzip -oq pac_bootstrap.zip
     rm pac_bootstrap.zip
     cd
     systemctl start Paccoin.service
-    sleep 60
-    paccoin-cli getinfo
+    # sleep 60
+    # paccoin-cli getinfo
     echo "Atualizado servico Pedro Pires" 
 else 
     cd
     killall paccoind
     sleep 20
     rm pacco*
-    wget https://github.com/PACCommunity/PAC/releases/download/v0.12.5.1/PAC-v0.12.5.1-linux-x86_64.tar.gz
-    tar -zvxf PAC-v0.12.5.1-linux-x86_64.tar.gz
-    rm PAC-v0.12.5.1-linux-x86_64.tar.gz
+    wget -q https://github.com/PACCommunity/PAC/releases/download/v0.12.6.0/PAC-v0.12.6.0-linux-x86_64.tar.gz
+    tar -zvxf PAC-v0.12.6.0-linux-x86_64.tar.gz
+    rm PAC-v0.12.6.0-linux-x86_64.tar.gz
     chmod +x pac*
     cd
     cd .paccoincore
@@ -68,18 +68,18 @@ else
     rm *.log
     rm -rf blocks
     rm -rf chainstate
-    wget https://www.dropbox.com/s/oaxp7875wy8kxpp/pac_bootstrap.zip
-    unzip pac_bootstrap.zip
+    wget -q https://www.dropbox.com/s/oaxp7875wy8kxpp/pac_bootstrap.zip
+    unzip -oq pac_bootstrap.zip
     rm pac_bootstrap.zip
     cd
     ./paccoind -daemon
     sleep 10
     cd
-    wget https://gist.githubusercontent.com/foxrtb/b703ae761472c5599c4d83ab0d3d62ae/raw/e8913deb9e1b7cc9c649febd2942930e4f6f5127/add-systemd-from-script
+    wget -q https://gist.githubusercontent.com/foxrtb/b703ae761472c5599c4d83ab0d3d62ae/raw/e8913deb9e1b7cc9c649febd2942930e4f6f5127/add-systemd-from-script
     chmod +x add-systemd-from-script
     ./add-systemd-from-script
     rm add-systemd-from-script
-    sleep 60
-    ./paccoin-cli getinfo
+    # sleep 60
+    " ./paccoin-cli getinfo
     echo "Atualizado e criado servio Geral" 
 fi
