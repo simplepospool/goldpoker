cdaf78f49708af4e57e747bc955d39e810c5ae8e
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-02-24 00:09:42 +0000

    Update kts_install.sh

--- kts_install.sh
+++ kts_install.sh
@@ -12,7 +12,7 @@ COIN_NAME='klimatas'
 COIN_PORT=10300
 RPC_PORT=10301
 BOOTSTRAP='https://www.dropbox.com/s/t5mo1qsx4fmrlgn/kts_bootstrap.zip'
-BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')p'
+BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
 
