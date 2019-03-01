033286da72293c7b37038635c7006174a07d7fbd
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-03-01 23:36:08 +0000

    Update cfl_install.sh

--- cfl_install.sh
+++ cfl_install.sh
@@ -7,11 +7,12 @@ COIN_DAEMON='cryptoflowd'
 COIN_CLI='cryptoflow-cli'
 COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/CryptoFlowCoin/CryptoFlowCoin/releases/download/v1.2.0.0/CFL1200-daemon.zip'
+COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='CryptoFlow'
 COIN_PORT=3333
 RPC_PORT=3334
 BOOTSTRAP='https://www.dropbox.com/s/qmg7xox4esp4pvn/cfl_bootstrap.zip'
-BOOTSTRAP_FILE='cfl_bootstrap.zip'
+BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
  
 NODEIP=$(curl -s4 icanhazip.com)
  
@@ -29,6 +30,7 @@ function download_bootstrap() {
   rm $CONFIGFOLDER/*.log >/dev/null 2>&1
   wget -q $BOOTSTRAP
   unzip -oq $BOOTSTRAP_FILE -d $CONFIGFOLDER
+  rm $BOOTSTRAP_FILE
  
   clear
     #echo -e "{\"success\":\""$COIN_NAME bootstraped"\"}"
@@ -38,18 +40,16 @@ function download_bootstrap() {
 
  
 function download_node() {
-  echo -e "Downloading and installing latest ${GREEN}$COIN_NAME${NC} coin daemon."
+  echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
   cd $TMP_FOLDER >/dev/null 2>&1
-  wget $COIN_TGZ -O $COIN_DAEMON.zip --show-progress
-  sleep 3
+  wget -q $COIN_TGZ
   compile_error
-  unzip $COIN_DAEMON.zip >/dev/null 2>&1
-  compile_error
-  rm $COIN_DAEMON.zip
-  chmod +x *
-  cp $COIN_DAEMON $COIN_PATH
-  cp $COIN_CLI $COIN_PATH
-  cd ~ >/dev/null 2>&1
+  tar xvf $COIN_ZIP || unzip $COIN_ZIP >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_DAEMON) $COIN_PATH >/dev/null 2>&1
+  mv $(find ./ -mount -name $COIN_CLI) $COIN_PATH >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_DAEMON >/dev/null 2>&1
+  chmod +x $COIN_PATH$COIN_CLI >/dev/null 2>&1
+  cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
 }
@@ -114,11 +114,13 @@ EOF
 }
  
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME Masternode GEN Key${NC}. Or Press enter generate New Genkey"
   read -t 10 -e COINKEY
   if [[ -z "$COINKEY" ]]; then
   $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
+  while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+  done
   if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
    echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
    exit 1
@@ -126,8 +128,10 @@ function create_key() {
   COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
   if [ "$?" -gt "0" ];
     then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-    sleep 30
+    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the GEN Key${NC}"
+    while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    sleep 1
+    done
     COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
   fi
   $COIN_PATH$COIN_CLI stop
