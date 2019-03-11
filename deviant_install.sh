6188e4b69c94d0b695d8201500544135bb7ed57e
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-03-11 21:14:30 +0000

    Update deviant_install.sh

--- deviant_install.sh
+++ deviant_install.sh
@@ -146,13 +146,6 @@ maxconnections=256
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
-addnode=209.97.131.147
-addnode=209.97.139.20
-addnode=209.97.139.2
-addnode=206.189.155.48
-addnode=167.99.234.81
-addnode=138.197.146.236
-addnode=209.97.131.20
 EOF
 }
 
