5bed96f10fa19e965942012a017963107e4e827c
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-03-12 18:36:16 +0000

    Update pac_install.sh

--- pac_install.sh
+++ pac_install.sh
@@ -153,7 +153,7 @@ clear
 function update_config() {
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 #bind=$NODEIP
-gen=1
+gen=0
 masternode=1
 masternodeaddr=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
