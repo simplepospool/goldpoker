b24fc56d3607dd8078eb4486d1eb993a393088e5
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-08-02 02:28:22 +0100

    Update 1x2_install.sh

--- 1x2_install.sh
+++ 1x2_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='1x2coin'
 COIN_PORT=9214
 RPC_PORT=9215
-BOOTSTRAP='https://www.dropbox.com/s/4wisofvkfff0bfj/npc_bootstrap.zip'
+BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -308,7 +308,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
