542d949d37ed5703c33d90796ed04514227a3511
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-08-02 03:51:47 +0100

    Update snode_install.sh

--- snode_install.sh
+++ snode_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='snodecoin'
 COIN_PORT=18988
 RPC_PORT=18989
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.112.107/snd-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -308,7 +308,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
