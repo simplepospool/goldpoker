434c3a477da5d30a35c72c4d8e6f086eb5a85504
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-08-02 03:02:48 +0100

    Update midas_install.sh

--- midas_install.sh
+++ midas_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='midas'
 COIN_PORT=44433
 RPC_PORT=44434
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.112.107/midas-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -310,7 +310,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
