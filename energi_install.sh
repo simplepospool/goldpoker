b058d6a0196b45d6c269f98e2a6ae1492958c5a0
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-08-03 02:45:11 +0100

    Update energi_install.sh

--- energi_install.sh
+++ energi_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='energi'
 COIN_PORT=9797
 RPC_PORT=9798
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.112.107/nrg-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 SENTINEL_REPO='https://github.com/energicryptocurrency/sentinel.git'
 
@@ -309,7 +309,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
