db7fc82180119c6e5f4b3984f81458213352a61a
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-08-24 18:31:19 +0100

    Update versess.sh

--- versess.sh
+++ versess.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='versess'
 COIN_PORT=13811
 RPC_PORT=13812
-BOOTSTRAP='https://github.com/VersessCTO/versess-core/releases/download/v1.0/bootstrap.zip'
+BOOTSTRAP='http://164.68.119.61/vers-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
