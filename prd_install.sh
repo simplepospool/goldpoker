0aba075be144fc89818c8c22b300c16f6296d7c6
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-08-31 00:16:44 +0100

    Update prd_install.sh

--- prd_install.sh
+++ prd_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='ParadiseCoin'
 COIN_PORT=45369
 RPC_PORT=45370
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.119.61/prd-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -308,7 +308,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
