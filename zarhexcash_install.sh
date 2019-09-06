9e75928f664948c0269c43075d80e6401dfa7c5e
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-09-06 03:43:40 +0100

    Update zarhexcash_install.sh

--- zarhexcash_install.sh
+++ zarhexcash_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='zarhexcash'
 COIN_PORT=40445
 RPC_PORT=40446
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.119.61/zarh-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -314,7 +314,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
