296f16c34a36120cf44753170cfe7fd86fc3c721
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-09-27 01:15:51 +0100

    Update dashd_install.sh

--- dashd_install.sh
+++ dashd_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='dashdiamond'
 COIN_PORT=12341
 RPC_PORT=12342
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.119.61/dashd-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -311,7 +311,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
