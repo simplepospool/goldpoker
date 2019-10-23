316304faefea62fce832a8def5d8b29dbbf87e40
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-10-23 18:30:51 +0100

    Update allsafe_install.sh

--- allsafe_install.sh
+++ allsafe_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='allsafe'
 COIN_PORT=7127
 RPC_PORT=7128
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.116.197/safe-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -334,7 +334,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
