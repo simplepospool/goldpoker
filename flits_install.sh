997026df0cd6943e668b3e6af0b1a478e7716bb8
simplepospool <38740186+simplepospool@users.noreply.github.com>
2019-11-24 22:04:44 +0000

    Update flits_install.sh

--- flits_install.sh
+++ flits_install.sh
@@ -9,8 +9,8 @@ COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/flitsnode/flits-core/releases/download/v1.0.1/fls-1.0.1-x86_64-linux-gnu.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='fls'
-COIN_PORT=12271
-RPC_PORT=12272
+COIN_PORT=12270
+RPC_PORT=12271
 BOOTSTRAP='http://164.68.112.107/fls-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
@@ -24,6 +24,21 @@ RED=''
 GREEN=""
 NC=''
 MAG=''
+
+function find_port() {
+        # <$1 = initial_check>
+
+        function port_check_loop() {
+                for (( i=$1; i<=$2; i++ )); do
+                        if [[ ! $(lsof -Pi :$i -sTCP:LISTEN -t) ]]; then
+                                echo $i
+                                return
+                        fi
+                done
+        }
+        local port=$(port_check_loop $1 $RPC_PORT)
+        [[ $port ]] && echo $port || echo $(port_check_loop 1024 $1)
+}
 
 purgeOldInstallation() {
     echo -e "${GREEN}Searching and removing old $COIN_NAME files and configurations${NC}"
@@ -130,12 +145,14 @@ function create_config() {
   cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASSWORD
-rpcport=$RPC_PORT
+rpcport=$(find_port $RPC_PORT)
 rpcallowip=127.0.0.1
+#------------------
 listen=1
 server=1
 daemon=1
 port=$COIN_PORT
+#------------------
 EOF
 }
 
@@ -149,13 +166,15 @@ function create_key() {
   done
   if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
    echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
+   echo -e "{\"error\":\"$COIN_NAME server couldn not start. Check /var/log/syslog for errors.\",\"errcode\":1098}"
    exit 1
   fi
   COINKEY=$(try_cmd $COIN_PATH$COIN_CLI "createmasternodekey" "masternode genkey")
   if [ "$?" -gt "0" ];
     then
     echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the GEN Key${NC}"
-    while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
+    echo -e "{\"error\":\"Wallet not fully loaded. Let us wait and try again to generate the GEN Key.\",\"errcode\":1099}"
+	while [[ ! $($COIN_CLI getblockcount 2> /dev/null) =~ ^[0-9]+$ ]]; do 
     sleep 1
     done
     COINKEY=$(try_cmd $COIN_PATH$COIN_CLI "createmasternodekey" "masternode genkey")
@@ -171,11 +190,13 @@ function update_config() {
 logintimestamps=1
 maxconnections=256
 #bind=$NODEIP
+#-----------------------------
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
+#-----------------------------
+#$COIN_NAME addnodes
 
-#fls addnodes
 addnode=195.46.187.71:12270
 addnode=142.93.233.108:12270
 addnode=165.22.203.61:12270
@@ -192,6 +213,22 @@ addnode=159.65.185.239:12270
 addnode=204.48.18.155:12270
 addnode=128.199.201.114:12270
 addnode=178.128.54.221:12270
+addnode=195.46.187.71:12270
+addnode=134.209.93.244:12270
+addnode=140.82.39.250:12270
+addnode=167.86.124.232:12270
+addnode=46.101.244.207:12270
+addnode=193.70.86.26:12270
+addnode=51.254.128.202:12270
+addnode=45.76.136.171:12270
+addnode=45.32.149.231:12270
+addnode=45.63.100.26:12270
+addnode=51.83.78.3:12270
+addnode=217.182.66.27:12270
+addnode=51.75.126.3:12270
+addnode=80.211.108.251:12270
+addnode=159.65.185.239:12270
+addnode=5.189.177.186:12270
 
 EOF
 }
@@ -235,6 +272,7 @@ function compile_error() {
 if [ "$?" -gt "0" ];
  then
   echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  echo -e "{\"error\":\"Impossible to locate the daemon\",\"errcode\":1100}"
   exit 1
 fi
 }
@@ -243,16 +281,19 @@ fi
 function checks() {
 if [[ $(lsb_release -d) != *16.04* ]]; then
   echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  echo -e "{\"error\":\"You´re not using Ubuntu 16.04\",\"errcode\":1101}"
   exit 1
 fi
 
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}$0 must be run as root.${NC}"
+   echo -e "{\"error\":\"$0 must be run as root\",\"errcode\":1103}"
    exit 1
 fi
 
 if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
   echo -e "${RED}$COIN_NAME is already installed.${NC} Please Run again.."
+  echo -e "{\"error\":\"$COIN_NAME is already installed. Please Run again..\",\"errcode\":1104}"
   exit 1
 fi
 }
@@ -275,6 +316,7 @@ libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip libzm
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
+	echo -e "{\"error\":\"Not all required packages were installed properly. Try to install them manually by running the following commands\",\"errcode\":1105}"
     echo "apt-get update"
     echo "apt -y install software-properties-common"
     echo "apt-add-repository -y ppa:bitcoin/bitcoin"
@@ -342,3 +384,10 @@ prepare_system
 download_node
 setup_node
 
+#169echo -e "{\"error\":\"$COIN_NAME server couldn not start. Check /var/log/syslog for errors.\",\"errcode\":1098}"
+#176echo -e "{\"error\":\"Wallet not fully loaded. Let us wait and try again to generate the GEN Key.\",\"errcode\":1099}"
+#243echo -e "{\"error\":\"Impossible to locate the daemon\",\"errcode\":1100}"
+#252echo -e "{\"error\":\"You´re not using Ubuntu 16.04\",\"errcode\":1101}"
+#258echo -e "{\"error\":\"$0 must be run as root\",\"errcode\":1103}"
+#264echo -e "{\"error\":\"$COIN_NAME is already installed. Please Run again..\",\"errcode\":1104}"
+#287echo -e "{\"error\":\"Not all required packages were installed properly. Try to install them manually by running the following commands\",\"errcode\":1105}"
