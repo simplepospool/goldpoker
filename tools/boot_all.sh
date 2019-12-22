2cd2dd1f316697e22eb782373c4c23a05d790296
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2019-12-22 01:36:55 +0000

    Create boot_all.sh

---
+++ tools/boot_all.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+
+echo -e "${GREEN}Please tell me what coin you want${NC}"
+read -e COIN
+
+echo -e "${GREEN}Tell me the bootstrap file${NC}"
+read -e ZIPFILE
+
+dupmn systemctlall $COIN stop
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/blocks
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/chainstate
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/database
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/backups
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/sporks
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/zerocoin
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/*.pid
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/*.log
+bash dupmn_all.sh $COIN rm -rf DUPFOLDER/*.dat
+bash dupmn_all.sh $COIN unzip -oq $ZIPFILE -d DUPFOLDER
+dupmn systemctlall $COIN start
+
