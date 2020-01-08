14fff85c2bb5d2f3f6da9d10ba4433c81e583af2
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2020-01-08 17:11:28 +0000

    Update safecapital_install.sh

--- safecapital_install.sh
+++ safecapital_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='safecapital'
 COIN_PORT=47002
 RPC_PORT=47003
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.116.197/scap-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -333,7 +333,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
