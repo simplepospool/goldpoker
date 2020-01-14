038138343023e2fcf6746a2f7159da3ed640ec45
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2020-01-14 01:15:24 +0000

    Update encocoinplus_install.sh

--- encocoinplus_install.sh
+++ encocoinplus_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='epgc'
 COIN_PORT=29442
 RPC_PORT=29443
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.116.197/epg-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -337,7 +337,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
