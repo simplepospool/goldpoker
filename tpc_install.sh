91c751de9781f27fe61c0a8805809bbfbce56a87
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2020-02-05 01:39:57 +0000

    Update tpc_install.sh

--- tpc_install.sh
+++ tpc_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='tpc'
 COIN_PORT=16521
 RPC_PORT=16522
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.116.197/tpc-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -342,7 +342,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
