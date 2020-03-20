d7d778315e81864ccd395edbc6ff1e779e82371e
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2020-03-20 17:38:58 +0000

    Update n808_install.sh

--- n808_install.sh
+++ n808_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='new808coin'
 COIN_PORT=33808
 RPC_PORT=33809
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.116.197/n808-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -334,7 +334,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
