1febf4e92d4a58a58cab551aacf78789d7401e8e
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2020-03-24 20:34:40 +0000

    Update xp_install.sh

--- xp_install.sh
+++ xp_install.sh
@@ -277,6 +277,10 @@ apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
 echo -e "Installing required packages, it may take some time to finish.${NC}"
 apt-get update >/dev/null 2>&1
 apt-get install libzmq3-dev -y >/dev/null 2>&1
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y >/dev/null 2>&1
+apt-get update -y >/dev/null 2>&1
+apt-get upgrade -y >/dev/null 2>&1
+apt-get install --only-upgrade libstdc++6 -y >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
