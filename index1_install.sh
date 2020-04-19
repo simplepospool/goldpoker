be65f74a2bbe6ab8ef0f9aa362d3212bede59f08
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2020-04-19 11:41:26 +0100

    Update index1_install.sh

--- index1_install.sh
+++ index1_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='index'
 COIN_PORT=7082
 RPC_PORT=7083
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.119.61/idx-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -339,7 +339,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
