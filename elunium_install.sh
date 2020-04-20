a4d0af8eccbec0cc9d674b58674e638755b9ded2
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2020-04-20 19:17:57 +0100

    Update elunium_install.sh

--- elunium_install.sh
+++ elunium_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='elunium'
 COIN_PORT=21500
 RPC_PORT=21501
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://164.68.116.197/elunium-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -339,7 +339,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
