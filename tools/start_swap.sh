330b3b9800586d1a001f4692f13191f983312890
simplepospool <38740186+simplepospool@users.noreply.github.com>
2020-06-23 01:55:41 +0100

    Update start_swap.sh

--- tools/start_swap.sh
+++ tools/start_swap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var; sudo touch swap.img; sudo chmod 600 swap.img; sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=20480; mkswap /var/swap.img; sudo swapon /var/swap.img; sudo free; sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+#cd /var; sudo touch swap.img; sudo chmod 600 swap.img; sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=20480; mkswap /var/swap.img; sudo swapon /var/swap.img; sudo free; sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
