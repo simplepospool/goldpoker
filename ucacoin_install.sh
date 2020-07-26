d70cfbe9f075fb33e14dff84d8780e4e2c4f699b
simplepospool <38740186+simplepospool@users.noreply.github.com>
2020-07-27 00:02:03 +0100

    Update ucacoin_install.sh

--- ucacoin_install.sh
+++ ucacoin_install.sh
@@ -11,8 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='ucacoin'
 COIN_PORT=33210
 RPC_PORT=33211
-#BOOTSTRAP='http://167.86.98.179/uca-bootstrap.zip'
-BOOTSTRAP='https://www.dropbox.com/s/m86sq1zix3k2jbv/uca_bootstrap.zip'
+BOOTSTRAP='http://167.86.107.173/uca-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
