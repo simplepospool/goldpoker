428010182cc461bea95f866a7becd7797261d1e0
simplepospool <38740186+simplepospool@users.noreply.github.com>
2020-07-26 23:52:50 +0100

    Update shb_install.sh

--- shb_install.sh
+++ shb_install.sh
@@ -11,7 +11,8 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='skyhub'
 COIN_PORT=25010
 RPC_PORT=51473
-BOOTSTRAP='http://167.86.98.179/shb-bootstrap.zip'
+#BOOTSTRAP='http://167.86.98.179/shb-bootstrap.zip'
+BOOTSTRAP='https://www.dropbox.com/s/8zjwjfkgwniyliw/shb_bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
  
 NODEIP=$(curl -s4 icanhazip.com)
