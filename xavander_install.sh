c80fd9f67f71898916282cce12eefff252d26103
simplepospool <38740186+simplepospool@users.noreply.github.com>
2020-08-22 18:38:42 +0100

    Update xavander_install.sh

--- xavander_install.sh
+++ xavander_install.sh
@@ -11,7 +11,7 @@ COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='xavander'
 COIN_PORT=35550
 RPC_PORT=35551
-BOOTSTRAP='http://164.68.112.107/1x2-bootstrap.zip'
+BOOTSTRAP='http://167.86.107.173/xczm-bootstrap.zip'
 BOOTSTRAP_FILE=$(echo $BOOTSTRAP | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
@@ -346,7 +346,7 @@ function try_cmd() {
 function setup_node() {
   get_ip
   create_config
-  #download_bootstrap
+  download_bootstrap
   create_key
   update_config
   enable_firewall
